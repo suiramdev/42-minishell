0bf92dc10ff1109cafabed28085fb2965c0fd429
suiramdev <mariusnouchet@gmail.com>
2023-04-26 17:49:41 +0200

    :sparkles: Added env command

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: mnouchet <mnouchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 16:34:03 by mnouchet          #+#    #+#             */
-/*   Updated: 2023/04/26 17:29:43 by mnouchet         ###   ########.fr       */
+/*   Updated: 2023/04/26 17:48:23 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define BUILTINS_H
 
 # include "types/command.h"
+# include "types/env.h"
 
 void	cmd_echo(t_cmd *cmd);
+void	cmd_env(t_cmd *cmd);
 
 #endif
--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,11 +6,24 @@
 /*   By: mnouchet <mnouchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:01:31 by mnouchet          #+#    #+#             */
-/*   Updated: 2023/04/26 16:51:01 by mnouchet         ###   ########.fr       */
+/*   Updated: 2023/04/26 17:48:51 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	cmd_env()
-{
+#include "types/command.h"
+#include "libft.h"
 
+void	cmd_env(t_cmd *cmd)
+{
+	char	**formatted_env;
+	size_t	i;
+
+	formatted_env = format_env(cmd->env);
+	i = 0;
+	while (formatted_env[i])
+		ft_putendl_fd(formatted_env[i++], STDOUT_FILENO);
+	i = 0;
+	while (formatted_env[i])
+		free(formatted_env[i++]);
+	free(formatted_env);
 }
