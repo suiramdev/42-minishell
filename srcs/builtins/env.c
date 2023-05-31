b47b4f49a335097431f78608b3bbf1b1d7a1f8b2
suiramdev <mariusnouchet@gmail.com>
2023-05-31 15:38:51 +0200

    :sparkles: Hide ? env in env cmd

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -12,6 +12,22 @@
 
 #include "minishell.h"
 
+static bool	is_special_env(char *key)
+{
+	char	*specials[1];
+	size_t	i;
+
+	specials[0] = "?";
+	i = 0;
+	while (i < 1)
+	{
+		if (!ft_strcmp(specials[i], key))
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
 /// @brief Execute the env builtin command
 /// @param cmd The command data structure
 /// @param envs The environment variables
@@ -24,7 +40,7 @@ int	builtin_env(t_cmd *cmd, t_env **envs)
 	env = *envs;
 	while (env)
 	{
-		if (env->value)
+		if (!is_special_env(env->key) && env->value)
 		{
 			if (printf("%s=%s\n", env->key, env->value) < 0)
 				return (error_write("env"), EXIT_FAILURE);
